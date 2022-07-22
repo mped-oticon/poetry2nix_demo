001bac2ea3947b6e1da2fd7daef7f7d2d28b9725
Mark Ruvald Pedersen <mped@oticon.com>
2022-07-23 01:24:58 +0200

    shell: Elaborate verbose explanation

--- shell.nix
+++ shell.nix
@@ -1,12 +1,30 @@
-{ pkgs ? import <nixpkgs> {}, allthepoetrystuff ? import ./default.nix {} }:
+{ pkgs ? import <nixpkgs> {},
+  allthepoetrystuff ? import ./default.nix {} 
+}:
 
 pkgs.mkShell {
 
   buildInputs = [
     pkgs.python3
     pkgs.poetry
-    allthepoetrystuff
-  ];
+    #allthepoetrystuff   # Hmm, this assumes lockfile is present
+  ]
+  # If absent, let's omit all the poetry stuff, but still give the user
+  # a chance to produce it - within a suitable shell containing poetry.
+  ++
+  (if builtins.pathExists ./poetry.lock then [ allthepoetrystuff ]
+  else []);
 
+  shellHook = 
+    if builtins.pathExists ./poetry.lock then ''
+      echo "Yay poetry.lock exists. I'll honor it"
+      echo "You have a perfectly usable environment now :)"
+    ''
+    else ''
+      echo "Boo! You don't have a lock file - so I'll make it now"
+      poetry lock || exit 1
+      echo "Lockfile created. But this nix-shell is unusable; start it again. Exiting for now."
+    ''
+  ;
 }
 
